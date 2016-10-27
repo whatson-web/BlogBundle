eb19c69995bab9756f56dedd88fbf8de38794d35
JF <jf@whatson-web.com>
2016-10-27 16:38:59 +0200

    conventions

---
+++ Controller/Frontend/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WH\BlogBundle\Controller\Frontend;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class PostController
+ *
+ * @package WH\BlogBundle\Controller\Frontend
+ */
+class PostController extends Controller
+{
+
+	/**
+	 * @param         $id
+	 * @param Request $request
+	 */
+	public function viewAction($id, Request $request)
+	{
+		dump($id);
+		dump($request);
+	}
+
+}
